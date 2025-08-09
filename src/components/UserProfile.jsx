792851e6ccdf23bcc95e23e2ef12c55fb382e776
Sujan Sah <sujansah.dev@gmail.com>
2025-08-09 16:48:26 +0545

    changes in profile picture image

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -6,7 +6,7 @@ export default function UserProfile({username,avatar}){
       {avatar ?  <img
          src={fileService.getAvatarView(avatar)}
          alt="avatar"
-        className="avatar  w-10 h-10 text-2xl rounded-full bg-slate-400/30 flex justify-center items-center capitalize"/>:
+        className="avatar  w-10 h-10 text-2xl rounded-full bg-slate-400/30 flex justify-center items-center capitalize object-center object-cover"/>:
         <span className="avatar  w-10 h-10 text-2xl rounded-full text-white bg-slate-400/30 flex justify-center items-center capitalize">{username.split("")[0]}</span>
         }
         <p className="capitalize">{username}</p>
